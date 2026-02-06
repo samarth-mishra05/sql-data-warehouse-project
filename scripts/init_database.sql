74b319d47138b1f5b85d9ddf112aece1afd9785d
Samarth Mishra <154755299+samarth-mishra05@users.noreply.github.com>
2026-02-07 00:10:49 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,24 @@
+/*
+================================================================
+Create Database and Schemas
+================================================================
+Script Purpose: 
+    This script creates a new database named 'DataWarehouse'.
+    Additionally, the script sets up three schemas within the database: 'bronze', 'silver', and 'gold'.
+*/
+
+--- Create Database 'DataWarehouse'
+
+CREATE DATABASE DataWarehouse
+GO
+
+---Create Schemas
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
 
